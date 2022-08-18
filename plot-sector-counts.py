28ebbc2e95c23973209f4e88d0d0732aac64ac57
Aidan Medcalf <aidanmedcalf@gmail.com>
2022-08-18 13:10:07 -0500

    Sector count combine and plotting

--- plot-sector-counts.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sys import argv
-from os import listdir
-
-directory_name = argv[1]
-print("Scanning directory", directory_name)
-for fname in listdir(directory_name):
-	print(fname)
